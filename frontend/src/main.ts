809150b40a11f3fbd8ed4060a7714dc336b1583d
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-02-25 13:28:09 +0700

    + add control jira module to reuse, svg, svg-symbol, button, input,
    avatar with variance

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .then(() => {
+        console.log('Application has been bootstrapped');
+    })
+    .catch((err) => {
+        console.error('Error bootstrapping the application', err);
+    });
