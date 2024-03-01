bc297882fdc6afa54bc746362720993785e27366
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-03-01 09:28:27 +0700

    + install ngrx packages, config module,
    + add json data, environment with apiUrl

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core";
+import { NgModule, isDevMode } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { RouterModule } from "@angular/router";
 import { routes } from "./app.routes";
@@ -12,6 +12,9 @@ import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 registerLocaleData(en);
 
 @NgModule({
@@ -24,7 +27,16 @@ registerLocaleData(en);
         ProjectModule,
         FormsModule,
         HttpClientModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        StoreModule.forRoot({
+        }),
+        EffectsModule.forRoot([]),
+        StoreDevtoolsModule.instrument({
+          maxAge: 25, 
+          logOnly: !isDevMode(),
+          autoPause: true,
+          traceLimit: 25
+        })
     ],
     bootstrap: [AppComponent],
     providers: [
